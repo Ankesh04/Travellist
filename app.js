c864df1086a1bc56dc49fd4f09ee9939fadf7435
Ankesh04 <osankeshagarwal4@gmail.com>
2026-04-20 18:37:05 +0530

    Completed first index rout

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+// ejs setup
+const path = require("path");
 
 // creatings database
 const MONGO_URL = "mongodb://127.0.0.1:27017/travellist";
-main().then(() => {
+
+main()
+.then(() => {
     console.log("My friend database is connected");
 })
 .catch(err => {
@@ -17,26 +21,36 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 }
 
+// connecting to ejs
+app.set("view Engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // creating api for our backend
 app.get("/", (req, res) => {
     res.send("Hi, my friend api is working.");
 });
 
-// creating new route
-app.get("/testListing", async (req, res) => {
-    // creating a  sample list for a model
-    let sampleListing = new Listing({
-        title: "My home",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("Testing successful")
+// new index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings})
 });
+
+// creating new route
+// app.get("/testListing", async (req, res) => {
+//     // creating a  sample list for a model
+//     let sampleListing = new Listing({
+//         title: "My home",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("Testing successful")
+// });
 
 app.listen(8080, () => {
     console.log("Server is ready my friend to port 8080.");
